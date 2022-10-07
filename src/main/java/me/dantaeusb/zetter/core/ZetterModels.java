ac73ed30307e008a708c14b3ff4883b61736cfae
Dmitry Burlakov <hi@dantaeusb.me>
2022-10-07 03:44:41 +0300

    Model register and configs

--- src/main/java/me/dantaeusb/zetter/core/ZetterModels.java
+++ src/main/java/me/dantaeusb/zetter/core/ZetterModels.java
@@ -6,8 +6,7 @@ import me.dantaeusb.zetter.client.renderer.entity.CustomPaintingRenderer;
 import me.dantaeusb.zetter.client.renderer.entity.EaselRenderer;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ForgeModelBakery;
+import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -16,9 +15,9 @@ public class ZetterModels
 {
     @SubscribeEvent
     @SuppressWarnings("unused")
-    public static void onModelRegistryEvent(ModelRegistryEvent event) {
+    public static void onModelRegistryEvent(ModelEvent.RegisterAdditional event) {
         for (ModelResourceLocation modelLocation : CustomPaintingRenderer.FRAME_MODELS.values()) {
-            ForgeModelBakery.addSpecialModel(modelLocation);
+            event.register(modelLocation);
         }
     }
 
