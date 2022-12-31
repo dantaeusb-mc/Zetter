7147edad27052ed5f9d7da14c33992c4a0363a59
Dmitry Burlakov <hi@dantaeusb.me>
2022-12-31 03:14:21 +0300

    #68 No need for that

--- src/main/java/me/dantaeusb/zetter/core/ZetterOverlays.java
+++ src/main/java/me/dantaeusb/zetter/core/ZetterOverlays.java
@@ -1,11 +1,7 @@
 package me.dantaeusb.zetter.core;
 
-import me.dantaeusb.zetter.Zetter;
 import me.dantaeusb.zetter.client.gui.overlay.PaintingInfoOverlay;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = Zetter.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ZetterOverlays {
     public static PaintingInfoOverlay PAINTING_INFO = new PaintingInfoOverlay();
 }
